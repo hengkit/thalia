cb87318a8c7d6f85156fed8397fea6c800e61d2d
Kit <kit@getpantheon.com>
2016-04-20 17:27:03 -0700

    testing login functionality

--- index.php
+++ index.php
@@ -1,13 +1,34 @@
 <?php
+
+require(__DIR__ . '/vendor/autoload.php');
+
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
-require 'vendor/autoload.php';
-use \Terminus\Commands\TerminusCommand;
+use \Terminus\Models\Auth;
+
+
+
+/*function logIn(array $options) {
+  if (!(is_null($options['token']) || is_null($options['email']))) {
+    $auth = new Auth();
+    $auth ->logInViaMachineToken($options['token'],$options['email']);
+  }
+}*/
 
 $app = new \Slim\App;
 
-// Define app routes
-$app->get('/terminus/version/', function ($request, $response, $args) {
+$app->post('/terminus/auth/login', function ($request, $response, $args) {
+    $authPayload = $request->getParsedBody();
+    $auth = new Auth();
+    $auth ->logInViaMachineToken($authPayload['authentication']['token'],$authPayload['authentication']['email']);
+    return $response->write($auth);
+});
+$app->get('/ping', function ($request, $response, $args) {
+    return $response->write("pong");
+});
+// Define app routes. Well that was naive
+
+/*$app->get('/terminus/cli/version/', function ($request, $response, $args) {
     return $response->write("terminus cli version stub");
 });
 $app->post('/terminus/site/env', function ($request, $response, $args) {
@@ -15,6 +36,7 @@ $app->post('/terminus/site/env', function ($request, $response, $args) {
 });
 $app->delete('/terminus/site/env', function ($request, $response, $args) {
     return $response->write("delete multidev stub " );
-});
+});*/
+
 // Run app
 $app->run();
