3bddbe70ddba0a0f2a2d1612538bcf948a5953b7
Kit <kit@getpantheon.com>
2016-06-15 16:08:14 -0700

    breakthrough with the runner

--- index.php
+++ index.php
@@ -5,15 +5,24 @@ define(TERMINUS_ROOT, __DIR__ . '/vendor/pantheon-systems/terminus/');
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 use \Terminus\Models\Auth;
+use \Terminus\Commands\AuthCommand;
+use \Terminus\Commands\TerminusCommand;
+use Terminus\Runner;
+use \Terminus\Commands\SiteCommand;
 use \Terminus\Session;
 
-$app = new \Slim\App;
-
+//$app = new \Slim\App;
+$app = new \Slim\App([
+    'settings' => [
+        'displayErrorDetails' => true
+    ]
+]);
+//login
 $app->post('/terminus/auth/login', function ($request, $response, $args) {
   try {
     $authPayload = $request->getParsedBody();
     $terminusArgs['email']=$authPayload['email'];
-    $terminusArgs['token'] = getenv('TERMINUS_TOKE');
+    $terminusArgs['token'] = getenv('TERMINUS_TOKEN');
     $auth = new Auth();
     $auth->logInViaMachineToken($terminusArgs);
     $response->withStatus(200);
@@ -24,21 +33,32 @@ $app->post('/terminus/auth/login', function ($request, $response, $args) {
   }
 
 });
-// There is no logout function, have to invalidate session
+// logout
 $app->post('/terminus/auth/logout', function ($request, $response, $args) {
     $auth = new Auth();
-    $auth->logout();
-    return $response->write($auth);
+    $auth->logOut();
+    return $response->withStatus(200);
 });
+//whoami
 $app->get('/terminus/auth/whoami', function ($request, $response, $args) {
-  if (Session::getValue('user_uuid')) {
-    $user = Session::getUser();
-    $user->fetch();
-    $data = $user->serialize();
-    $response->withJson($data);
-  } else {
-    $response->withStatus(403);
-  }
+  $authCommand = new AuthCommand(['runner' => new Runner()]);
+  $me = $authCommand->whoami();
+  $response->withJson($me);
+  return $response;
+});
+#/terminus/site/{siteName}/env/{envName}
+//terminus site info
+$app->get('/terminus/site/{siteName}/', function ($request, $response, $args) {
+  $siteName=['siteName'=>$args['siteName']];
+  $site=new SiteCommand();
+  $siteinfo = $site->info($siteName);
+  $response->withJson($siteinfo);
+
+  return $response;
+});
+
+$app->post('/terminus/site/{siteName}/env/{envName}', function ($request, $response, $args) {
+
   return $response;
 });
 $app->get('/ping', function ($request, $response, $args) {
