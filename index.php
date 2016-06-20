561875182cc043d54cf8d7038f8824d7953d2616
Kit <kit@getpantheon.com>
2016-06-20 16:50:43 -0700

    finally got the runner figured out

--- index.php
+++ index.php
@@ -10,12 +10,19 @@ use \Terminus\Commands\TerminusCommand;
 use Terminus\Runner;
 use \Terminus\Commands\SitesCommand;
 
-//$app = new \Slim\App;
+if(!defined('STDIN'))  define('STDIN',  fopen('php://input',  'r'));
+if(!defined('STDOUT')) define('STDOUT', fopen('php://output', 'w'));
+if(!defined('STDERR')) define('STDERR', fopen('php://stderr', 'w'));
+
 $app = new \Slim\App([
     'settings' => [
-        'displayErrorDetails' => true
+        'displayErrorDetails' => false,
+        'defaultRunner'=>[
+          'runner' => new Runner(array('format'=> 'json','output'=>'php://output'))
+        ]
     ]
 ]);
+
 //terminus auth login
 $app->post('/terminus/auth/login', function ($request, $response, $args) {
   try {
@@ -40,17 +47,19 @@ $app->post('/terminus/auth/logout', function ($request, $response, $args) {
 });
 //terminus auth whoami
 $app->get('/terminus/auth/whoami', function ($request, $response, $args) {
-  $authCommand = new AuthCommand(['runner' => new Runner()]);
+  //$myRunner=['runner' => new Runner(array('format'=> 'json','output'=>'php://output'))];
+  $authCommand = new AuthCommand($this->get('settings')['defaultRunner']);
   $me = $authCommand->whoami();
-  $response->withJson($me);
+  $response = json_decode($me);
+  //$response->withJson($me);
   return $response;
 });
 //terminus sites show --name
 $app->get('/terminus/sites/{siteName}/', function ($request, $response, $args) {
-  $sitesCommand = new SitesCommand(['runner' => new Runner()]);
+  $sitesCommand = new SitesCommand($this->get('settings')['defaultRunner']);
   $meh=null;
   $info = $sitesCommand->index($meh,['name'=>$args[siteName]]);
-  $response->withJson($info);
+  $response= json_decode($info);
   return $response;
 });
 
